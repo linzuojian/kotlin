078a3a19206bb1285c4614cfcbf9c9f08eded385
Adam Semenenko <adam.semenenko@jetbrains.com>
2026-05-20 20:04:17 +0200

    [Gradle] Deduplicate fetching KotlinJsIrCompilation npm tooling dir
    Create helper function to deduplicate fetching KGP's npm tooling dir.
    
    Preparatory step for KT-84724
    
    ### What is the npm tooling dir?
    
    KGP requires npm tooling dependencies for development tasks
    (like bundling, testing, running the dev server).
    
    For JS projects KGP's tooling dependencies are installed in
    the same npm project as user-defined dependencies.
    For WasmJS projects, they're installed in a global directory.

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/nodejs/NodeJsExec.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/nodejs/NodeJsExec.kt
@@ -5,6 +5,8 @@
 
 package org.jetbrains.kotlin.gradle.targets.js.nodejs
 
+import org.gradle.api.file.Directory
+import org.gradle.api.provider.Provider
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.Property
@@ -16,14 +18,15 @@ import org.jetbrains.kotlin.gradle.targets.js.KotlinWasmTargetType
 import org.jetbrains.kotlin.gradle.targets.js.NpmVersions
 import org.jetbrains.kotlin.gradle.targets.js.RequiredKotlinJsDependency
 import org.jetbrains.kotlin.gradle.targets.js.ir.KotlinJsIrCompilation
+import org.jetbrains.kotlin.gradle.targets.js.ir.nodeJsRoot
+import org.jetbrains.kotlin.gradle.targets.js.ir.npmToolingDir
 import org.jetbrains.kotlin.gradle.targets.js.npm.NpmProjectModules
 import org.jetbrains.kotlin.gradle.targets.js.npm.RequiresNpmDependenciesTask
 import org.jetbrains.kotlin.gradle.targets.js.npm.npmProject
-import org.jetbrains.kotlin.gradle.targets.js.npm.npmToolingDir
+import org.jetbrains.kotlin.gradle.targets.js.webTargetVariant
 import org.jetbrains.kotlin.gradle.targets.wasm.internal.isWasm
+import org.jetbrains.kotlin.gradle.targets.wasm.nodejs.WasmNodeJsPlugin
 import org.jetbrains.kotlin.gradle.targets.wasm.nodejs.WasmNodeJsRootExtension
-import org.jetbrains.kotlin.gradle.targets.web.nodejs.nodeJsEnvSpec
-import org.jetbrains.kotlin.gradle.targets.web.nodejs.nodeJsRoot
 import org.jetbrains.kotlin.gradle.tasks.registerTask
 import org.jetbrains.kotlin.gradle.utils.getFile
 import org.jetbrains.kotlin.gradle.utils.newFileProperty
@@ -111,11 +114,21 @@ constructor(
         ): TaskProvider<NodeJsExec> {
             val target = compilation.target
             val project = target.project
-            val nodeJsRoot = compilation.nodeJsRoot
-            val nodeJsEnvSpec = compilation.nodeJsEnvSpec
+
+            val nodeJsRoot = compilation.nodeJsRoot()
+            val nodeJsEnvSpec = compilation.webTargetVariant(
+                { NodeJsPlugin.apply(project) },
+                { WasmNodeJsPlugin.apply(project) },
+            )
 
             val npmProject = compilation.npmProject
-            val npmToolingDir = compilation.npmToolingDir
+
+            val npmToolingDir: Provider<Directory> = compilation.npmToolingDir()
+
+            val isWasm: Boolean = compilation.webTargetVariant(
+                jsVariant = false,
+                wasmVariant = true,
+            )
 
             return project.registerTask(
                 name,
