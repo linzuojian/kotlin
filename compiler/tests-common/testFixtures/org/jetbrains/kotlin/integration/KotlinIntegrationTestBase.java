4c24306a92a4e640699edc5042f6470024188ae1
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2026-06-16 17:14:30 +0200

    Tests: replace deprecated ProcessAdapter with ProcessListener
    Caused by KT-82657

--- compiler/tests-common/testFixtures/org/jetbrains/kotlin/integration/KotlinIntegrationTestBase.java
+++ compiler/tests-common/testFixtures/org/jetbrains/kotlin/integration/KotlinIntegrationTestBase.java
@@ -18,10 +18,7 @@ package org.jetbrains.kotlin.integration;
 
 import com.intellij.execution.ExecutionException;
 import com.intellij.execution.configurations.GeneralCommandLine;
-import com.intellij.execution.process.OSProcessHandler;
-import com.intellij.execution.process.ProcessAdapter;
-import com.intellij.execution.process.ProcessEvent;
-import com.intellij.execution.process.ProcessOutputTypes;
+import com.intellij.execution.process.*;
 import com.intellij.openapi.util.Key;
 import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.util.io.FileUtil;
@@ -163,7 +160,7 @@ public abstract class KotlinIntegrationTestBase extends TestCaseWithTmpdir {
         return distPath != null ? new File(distPath).getParentFile() : new File(KtTestUtil.getHomeDirectory());
     }
 
-    private static class OutputListener extends ProcessAdapter {
+    private static class OutputListener implements ProcessListener {
         private final StringBuilder out;
         private final StringBuilder err;
 
@@ -184,8 +181,5 @@ public abstract class KotlinIntegrationTestBase extends TestCaseWithTmpdir {
                 out.append(event.getText());
             }
         }
-
-        @Override
-        public void processTerminated(@NotNull ProcessEvent event) {}
     }
 }
