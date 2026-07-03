e31ff6c130f24357074cdb63c637c8b1cf0d229e
Pavel Kunyavskiy <pavel.kunyavskiy@jetbrains.com>
2026-07-03 09:57:08 +0200

    Get rid of K1 deprecations in ir.tree module

--- compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/declarations/IrPackageFragments.kt
+++ compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/declarations/IrPackageFragments.kt
@@ -14,6 +14,7 @@ import org.jetbrains.kotlin.ir.declarations.impl.IrFileImpl
 import org.jetbrains.kotlin.ir.symbols.impl.IrExternalPackageFragmentSymbolImpl
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.K1Deprecation
+import org.jetbrains.kotlin.serialization.deserialization.descriptors.DescriptorWithContainerSource
 import org.jetbrains.kotlin.serialization.deserialization.descriptors.DeserializedContainerSource
 import org.jetbrains.kotlin.serialization.deserialization.descriptors.DeserializedMemberDescriptor
 
@@ -24,8 +25,7 @@ val IrPackageFragment.packageFragmentDescriptor: PackageFragmentDescriptor
 
 @OptIn(ObsoleteDescriptorBasedAPI::class)
 val IrExternalPackageFragment.containerSource: DeserializedContainerSource?
-    @OptIn(K1Deprecation::class)
-    get() = (symbol.descriptor as? DeserializedMemberDescriptor)?.containerSource
+    get() = (symbol.descriptor as? DescriptorWithContainerSource)?.containerSource
 
 /**
  * This should be a link to [IrModuleFragment] instead.
